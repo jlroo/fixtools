b0c30b4592bacdd62b3488055c1553c242e98254
jose luis rodriguez <jrodriguezorjuela@luc.edu>
2016-08-03 17:00:28 -0500

    fixed import error

--- fixtools/__init__.py
+++ fixtools/__init__.py
@@ -1,6 +1,10 @@
-__all__ = ['fixtools']
+# pylint: disable-msg=W0614,W0401,W0611,W0622
+
+# flake8: noqa
+
 __docformat__ = 'restructuredtext'
 
+# Let users know if they're missing any of our hard dependencies
 hard_dependencies = ("gzip", "re")
 missing_dependencies = []
 
@@ -13,3 +17,5 @@ for dependency in hard_dependencies:
 if missing_dependencies:
     raise ImportError("Missing required dependencies {0}".format(missing_dependencies))
 del hard_dependencies, dependency, missing_dependencies
+
+from fixtools.util.tools import *
