c7aa9fe114876b62c82c134f6ed85f9b044cbee4
jose luis rodriguez <jrodriguezorjuela@luc.edu>
2016-08-03 19:00:43 -0500

    fixed path

--- fixtools/__init__.py
+++ fixtools/__init__.py
@@ -16,5 +16,5 @@ if missing_dependencies:
     raise ImportError("Missing required dependencies {0}".format(missing_dependencies))
 del hard_dependencies, dependency, missing_dependencies
 
-from fixtools.tools.util import (contracts,group_by,
-                                 periods,read_fix, to_day)
+from fixtools.util import (contracts,group_by,
+                                 periods,read_fix,to_day)
