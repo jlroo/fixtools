62dd9da1e169e2183b909a9bbf853956c94e5177
jrodriguezorjuela <jlrodriguezorjuela@luc.edu>
2017-03-13 18:10:00 -0500

    updateBook fixed

---
+++ build/lib/fixtools/__init__.py
@@ -0,0 +1,19 @@
+
+__all__ = ['FixData', 'openFix']
+__docformat__ = 'restructuredtext'
+
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ("gzip","bz2")
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(dependency)
+
+if missing_dependencies:
+    raise ImportError("Missing required dependencies {0}".format(missing_dependencies))
+del hard_dependencies, dependency, missing_dependencies
+
+from fixtools.util import (FixData,openFix,settlementDay,mostLiquid)
