5132a1d48a131daf11801d6f9a3ad73f7ca93858
jlroo <jrodriguezorjuela@luc.edu>
2017-09-07 13:55:30 -0500

    updated run.py script

--- run.py
+++ run.py
@@ -7,3 +7,8 @@ path = "/home/jlroo/data/2012/XCME_MD_ES_20120102_20120106.gz"
 fixdata = fx.open_fix(path)
 securities = fx.liquid_securities(fixdata)
 books = fx.build_books(fixdata, securities)
+
+for sec_id in books.keys():
+    with open(securities[sec_id].replace(" ","_"),'wb') as book_out:
+        for book in books[sec_id]:
+            book_out.write(book)
