72536faf88b0c0dedf487ca38e7ae2ae1a98cf5d
jose Luis Rodriguez <jrodriguezorjuela@luc.edu>
2018-02-26 20:56:52 -0600

    Version Update v2.0 - Modified API - New DataBook class

--- fixtools/core/book.py
+++ fixtools/core/book.py
@@ -10,6 +10,14 @@ import multiprocessing as __mp__
 from collections import defaultdict
 
 
+def __filter__( line_desc=tuple ):
+    valid_contract = [sec if sec in line_desc[0] else None for sec in line_desc[1]]
+    set_ids = filter(None , valid_contract)
+    security_ids = set(int(sec.split(b'\x0148=')[1].split(b'\x01')[0]) for sec in set_ids)
+    if b'35=X\x01' in line_desc[0] and any(valid_contract):
+        return security_ids , line_desc[0]
+
+
 class DataBook:
     path_out = None
     __fileOut__ = None
@@ -22,17 +30,10 @@ class DataBook:
         contract_ids = set(securities.keys())
         self.__securityDesc__ = [b'\x0148=' + str(sec_id).encode() + b'\x01' for sec_id in contract_ids]
 
-    def __filter__( self , line ):
-        valid_contract = [sec if sec in line else None for sec in self.__securityDesc__]
-        set_ids = filter(None , valid_contract)
-        security_ids = set(int(sec.split(b'\x0148=')[1].split(b'\x01')[0]) for sec in set_ids)
-        if b'35=X\x01' in line and any(valid_contract):
-            return security_ids , line
-
     def filter( self ):
         messages = defaultdict(list)
         with __mp__.Pool() as pool:
-            filtered = pool.map(self.__filter__ , self.data , self.chunksize)
+            filtered = pool.map(__filter__ , (self.data , self.__securityDesc__) , self.chunksize)
             for set_ids , line in filter(None , filtered):
                 for security_id in set_ids:
                     messages[security_id].append(line)
