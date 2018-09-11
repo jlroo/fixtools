c138872381babfcf50305ad6a6bb226fa2a96191
jrodriguezorjuela <jrodriguezorjuela@luc.edu>
2018-09-11 15:18:02 -0500

    refactoring

--- scripts/run-book.py
+++ scripts/run-book.py
@@ -16,26 +16,32 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file' , dest='file_path' , help='Fix data file input')
+    parser.add_argument('--path_out' , dest='path_out' , help='Fix data file output')
     parser.add_argument('--year_code' , dest='year_code' , help='Fix data year code')
     parser.add_argument('--data_out' , dest='data_out' , help='Fix books path out')
-    parser.add_argument('--compression' , dest='compression' , default=False ,
-                        type=lambda x: (str(x).lower() == 'true') , help='True/False flag compression')
-    parser.add_argument('--process' , dest='chunksize' , help='Number of threads')
-    parser.add_argument('--chunksize' , dest='chunksize' , help='Data chunksize')
-
+    parser.add_argument('--compression', dest='compression', action='store_true')
+    parser.add_argument('--no-compression', dest='compression', action='store_false')
+    parser.add_argument('--process' , dest='process' , help='Number of threads')
+    parser.add_argument('--book_process' , dest='book_process' , help='Number of threads')
+    parser.add_argument('--chunksize_filter' , dest='chunksize_filter' , help='data chunksize')
+    parser.add_argument('--chunksize_book' , dest='chunksize_book' , help='data chunksize')
+    parser.set_defaults(compression=True)
     args = parser.parse_args()
-
     fixdata = fx.open_fix(path=args.file_path , compression=args.compression)
-    data_lines = fixdata.data.readlines(10000)
+    data_lines = []
+    for n,line in enumerate(fixdata.data):
+        if n >=10000:
+            break
+        data_lines.append(line)
     fixdata.data.seek(0)
-    opt_code = fx.most_liquid(data_line=data_lines[0] , instrument="ES" , product="OPT" , code_year=args.year_code)
-    fut_code = fx.most_liquid(data_line=data_lines[0] , instrument="ES" , product="FUT" , code_year=args.year_code)
-    liquid_secs = fx.liquid_securities(data_lines , code_year=args.year_code)
-
-    filename = fut_code[2] + opt_code[2] + "-"
-    path_out = args.data_out + filename
-    fx.data_book(data=fixdata.data , securities=liquid_secs , path=path_out , chunksize=int(args.chunksize))
-
+    opt_code = fx.most_liquid(data_line=data_lines[0], instrument="ES", product="OPT", code_year=args.year_code)
+    fut_code = fx.most_liquid(data_line=data_lines[0], instrument="ES", product="FUT", code_year=args.year_code)
+    liquid_secs = fx.liquid_securities(data_lines, code_year=args.year_code)
+    desc_path = args.data_out + fut_code[2] + "/"
+    filename = str(0).zfill(3) + "-" + fut_code[2] + opt_code[2] + "-"
+    path_out = desc_path + filename
+    fx.data_book(data=fixdata.data, securities=liquid_secs, path=path_out, processes=args.processes, 
+                    chunksize_filter=args.chunksize_filter, chunksize_book=args.chunksize_book)
 
 if __name__ == '__main__':
     main()
